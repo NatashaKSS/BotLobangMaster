a43e61aae64b29ae11dfb8d011714e3ecbc0dcad
NatashaKSS <xnagol@gmail.com>
2017-02-25 22:29:47 +0800

    Added standard express and node modules

---
+++ src/BotLobangMaster.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+/**
+ * This class defines BotHub - The IQ of TaxiBot.
+ *
+ * Performs logical computation of user commands and interacts with third-party
+ * APIs (e.g. API.ai and Dashbot.io).
+ */
+module.exports = class BotHub {
+  constructor () {
+    this._app = express();
+
+    // Set up SecurityHandler
+    let SecurityHandler = require('./verification.js');
+    this._securityHandler = new SecurityHandler(this._app);
+  }
+
+  run() {
+    let app = this._app;
+    this._app.set('port', (process.env.PORT || 5005));
+
+    // Index route
+    this._app.get('/', function (req, res) {
+    	res.send('Hello World! I am BotLobangMaster!');
+    })
+
+    // Check if all config keys exist since we need every single one of them
+    this._securityHandler.allConfigsExist();
+
+    // Set up Express server middleware stack
+    this.setupMiddleware();
+
+    // Spin up the server
+    this._app.listen(this._app.get('port'), function () {
+      console.log('RUNNING ON PORT', app.get('port'));
+    })
+  }
+
+  setupMiddleware() {
+    // Serve static files in the public directory
+    this._app.use(express.static('public'));
+
+    // Process application/x-www-form-urlencoded
+    this._app.use(bodyParser.urlencoded({
+    	extended: false
+    }))
+
+    // Process application/json
+    this._app.use(bodyParser.json())
+  }
+}
