9d9a033b19b294174e40461c57181e4334f52319
NatashaKSS <xnagol@gmail.com>
2017-04-01 17:55:25 +0800

    Added documentation to Evaluator.js

--- src/NLP/Evaluator.js
+++ src/NLP/Evaluator.js
@@ -7,6 +7,13 @@ module.exports = class Evaluator {
 
   }
 
+  /**
+   * Prints Accuracy Report to console for a set of classification results
+   * against the expected classification results.
+   *
+   * @param {[Array]} actual Array of classification results generated
+   * @param {[Array]} expected Array of gold standard results to compare against
+   */
   printClassifierAccuracyReport(actual, expected) {
     let total = actual.length;
     let numCorrect = 0;
@@ -24,6 +31,16 @@ module.exports = class Evaluator {
     this.printSummary(total, numCorrect, numFalsePositives);
   }
 
+  /**
+   * Prints a Summary of the classification results to console.
+   * Summarizes:
+   *   Accuracy Rate, Misclassification Rate,
+   *   Number of False Positives out of the number of misclassified.
+   *
+   * @param {Integer} total Total number of results
+   * @param {Integer} numCorrect Number of results that were true positives
+   * @param {Integer} numFalsePositives Number of false positives
+   */
   printSummary(total, numCorrect, numFalsePositives) {
     console.log("================SUMMARY================");
     console.log("You got " + this.getAccuracyInStr(total, numCorrect) + " correct");
@@ -33,6 +50,15 @@ module.exports = class Evaluator {
     console.log("=============END OF SUMMARY============");
   }
 
+  /**
+   * Prints a small diagnostic summary of the misclassified data point.
+   * Gives diagnostic info about the original text, normalized tokens,
+   * expected label VS actual classified label and the computed probabilities
+   * of the 'promo' and 'not=promo' labels
+   *
+   * @param {Object} actualClassification Actual classification of this data point
+   * @param {Object} expectedEntry Expected gold standard of this data point
+   */
   printMisclassifedDetails(actualClassification, expectedEntry) {
     console.log("==============GOT THIS WRONG=============");
     console.log("Original Text: " + JSON.stringify(expectedEntry['originalMsg']));
@@ -43,10 +69,22 @@ module.exports = class Evaluator {
     console.log("=========================================");
   }
 
+  /**
+   * Determines whether or not this classification matches the gold standard.
+   *
+   * @param {Object} actualClassification Actual classification of this data point
+   * @param {Object} expectedEntry Expected gold standard of this data point
+   */
   isClassifiedCorrectly(actualClassification, expectedEntry) {
     return actualClassification['labels'][0]['label'] === expectedEntry['label'];
   }
 
+  /**
+   * Computes and returns the probabilities of the respective
+   * 'promo' and 'not-promo' labels
+   *
+   * @param {Object} actualClassification Actual classification of this data point
+   */
   getLabelProbabilities(actualClassification) {
     let labels = actualClassification['labels'];
     let labelProbabilities = {
@@ -65,6 +103,14 @@ module.exports = class Evaluator {
     return labelProbabilities;
   }
 
+  /**
+   * Determines if the actual classification object is a false positive or not.
+   * If it isn't a false positive, it must then be a false negative beacuse
+   * the actual and expected classification object are misclassified already.
+   *
+   * @param {Object} actualClassification Actual classification of this data point
+   * @param {Object} expectedEntry Expected gold standard of this data point 
+   */
   isFalsePositive(actualClassification, expectedEntry) {
     if (!this.isClassifiedCorrectly(actualClassification, expectedEntry)) {
       if (actualClassification['labels'][0]['label'] === 'promo') {
