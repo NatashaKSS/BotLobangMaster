c579fd1839d75e664a29dfe4af8b9ffdf125b655
NatashaKSS <xnagol@gmail.com>
2017-04-01 21:45:11 +0800

    Added Title and Description classes

---
+++ src/promo_machine/TitleConstructor.js
@@ -0,0 +1,29 @@
+/**
+ * Extracts title information from a promo's text field and
+ * Puts it in a format fit for for pushing to the chatbot
+ */
+module.exports = class TitleConstructor {
+  constructor() {
+    this._maxChars = 80;
+    this._titleFields = {
+
+    }
+  }
+
+  /**
+   * Best-effort extracts elements of a piece of text that most fits the
+   * 'title' category and returns
+   *
+   * @param {String} promoText Message text of the promo
+   * @return {String} Title of this promo
+   */
+  getTitle(promoText) {
+
+  }
+
+  //==============================================================
+  // 
+  //==============================================================
+
+
+}
