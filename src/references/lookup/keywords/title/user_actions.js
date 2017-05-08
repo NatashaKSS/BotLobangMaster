88754f04d639843c76497abf1f9b4eefa5abd147
NatashaKSS <xnagol@gmail.com>
2017-05-08 22:23:18 +0800

    Added user actions

---
+++ src/references/lookup/keywords/title/user_actions.js
@@ -0,0 +1,10 @@
+/**
+ * Dictionary for terms that most likely refer to user actions
+ */
+module.exports = {
+  keyword_singular: "ride",
+  keyword_plural: "rides",
+  terms: {
+    "take_one_ride": ["take one", "take 1"],
+  }
+}
