5b76522692cf2ebc16836bf5afd42d7abcaa41e8
NatashaKSS <xnagol@gmail.com>
2017-05-10 04:34:20 +0800

    Added more payment methods

--- src/references/lookup/keywords/title/user_actions.js
+++ src/references/lookup/keywords/title/user_actions.js
@@ -7,6 +7,12 @@ module.exports = {
   terms: {
     "take_one_ride": ["take one", "take 1"],
     "pay_with": ["pay with", "pay by", "pay via", "use", "using"],
-    "credit_cards": ["Grabpay", "GrabPay", "DBS", "POSB", "OCBC", "card", "your card"],
+    "credit_cards": [
+      "Grabpay", "GrabPay",
+      "DBS", "POSB", "OCBC",
+      "ApplePay", "ApplePAY", "Applepay",
+      "AndroidPay", "AndroidPAY", "Androidpay",
+      "card", "your card"
+    ],
   }
 }
