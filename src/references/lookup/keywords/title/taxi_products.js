3d228e2e66653f15fcf5d6cec4956280c278ffdd
NatashaKSS <xnagol@gmail.com>
2017-05-09 22:35:13 +0800

    Moved taxi products dictionary to keywords folder

---
+++ src/references/lookup/keywords/title/taxi_products.js
@@ -0,0 +1,15 @@
+/**
+ * Uber, Grab and Comfort's flagship product names
+ * PLEASE MAKE SURE NAMES ARE ALL IN SMALL LETTERS
+ */
+module.exports = {
+  keyword_singular: "taxi",
+  keyword_singular_cdg: "taxis",
+  terms: {
+    UBER: ["uberx", "uberxl", "uberexec", "execlarge", "ubertaxi", "uberpool", "pool"],
+    GRAB: ["grabcar", "grabtaxi", "grabhitch", "grabshare", "justgrab", "grabfamily"],
+    COMFORT: [],
+  },
+  ignore_terms: {
+  }
+}
