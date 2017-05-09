f187eb7aef87ed87c930a0fa16981c196a7fdc9e
NatashaKSS <xnagol@gmail.com>
2017-05-10 00:19:54 +0800

    Added send promos to AirTable functionality
    * Duplicate detection on AirTable using promo cod

--- src/promo_machine/PromoAirTableHandler.js
+++ src/promo_machine/PromoAirTableHandler.js
@@ -12,45 +12,41 @@ module.exports = class PromoAirTableHandler {
     this._base = this._airtable.base(this._baseID);
   }
 
-  sendToAirTable(table, promos, title, description,) {
+  sendToAirTable(table, promos) {
     let allAirTablePromos = promos;
 
-    this.getUniquePromos(promos, table, (allPromos) => {
-      allAirTablePromos = allPromos;
+    this.getUniquePromos(promos, table, (uniquePromos) => {
+      allAirTablePromos = uniquePromos;
 
       // allAirTablePromos should not contain duplicates at this point
       for(var i = 0; i < allAirTablePromos.length; i++) {
-        let promo = allAirTablePromos[i];
+        let promo = allAirTablePromos[i].promoObj;
+        let title = allAirTablePromos[i].title;
+        let description = allAirTablePromos[i].description;
 
-        if (!this.isExpired(promo)) {
-          this._base(table).create({
-            promo_code: promo.promo_code,
-            title: title,
-            description: description,
-            image: null,
-            brand: promo.brand,
-            product: JSON.stringify(promo.product),
-            amount_$: JSON.stringify(promo["amount_$"]),
-            "amount_%": JSON.stringify(promo["amount_%"]),
-            ride_type: JSON.stringify(promo["ride_type"]),
-            redemptions: JSON.stringify(promo["redemptions"]),
-            user_type: JSON.stringify(promo["user_type"]),
-            "Start Date & Time": this.getValidDate(promo["date"]["Start Date & Time"]),
-            "End Date & Time": promo["date"]["End Date & Time"],
-          }, (err, record) => {
-            if (!err) {
-              // No errors
-            } else {
-              // Return debug message with first 30 chars of post title
-              let postTitleSnippet = promo.post_title.slice(0, 30);
-              if (postTitleSnippet) {
-                console.log("In " + table + " table, " + promo.post_title.slice(0, 30) + "... " + err);
-              } else {
-                console.log("In " + table + " table, " + promo.post_title + "... " + err);
-              }
-            }
-          });
-        }
+        this._base(table).create({
+          promo_code_ID: promo.promo_code[0],
+          promo_codes: JSON.stringify(promo.promo_code),
+          title: title,
+          description: description,
+          image: null,
+          brand: promo.brand,
+          product: JSON.stringify(promo.product),
+          amount_$: JSON.stringify(promo["amount_$"]),
+          "amount_%": JSON.stringify(promo["amount_%"]),
+          ride_type: JSON.stringify(promo["ride_type"]),
+          redemptions: JSON.stringify(promo["redemptions"]),
+          user_type: JSON.stringify(promo["user_type"]),
+          "Start Date & Time": this.getValidDate(promo["date"]["Start Date & Time"]),
+          "End Date & Time": promo["date"]["End Date & Time"],
+        }, (err, record) => {
+          if (!err) {
+            // No errors
+          } else {
+            // Return debug message with first 30 chars of post title
+            console.log("ERROR IN", record);
+          }
+        });
       }
       console.log("DONE WITH SENDING TO AIRTABLE. PLEASE CHECK AIRTABLE.");
     });
@@ -65,7 +61,7 @@ module.exports = class PromoAirTableHandler {
       // This function (`page`) will get called for each page of records.
       records.forEach(function(record) {
         // Skip a promo that's already in AirTable
-        allPromos = this.removeDuplicatePromo(allPromos, record.get('promo_code'));
+        allPromos = this.removeDuplicatePromo(allPromos, record.get('promo_code_ID'));
       }.bind(this));
 
       // To fetch the next page of records, call `fetchNextPage`.
@@ -83,10 +79,18 @@ module.exports = class PromoAirTableHandler {
     }.bind(this));
   }
 
+  /**
+   * Check for duplicates by using the first promo code extracted from a
+   * promo object and cross-check that with promo_code_ID of AirTable
+   *
+   * @param  {[Array]} promos             List of promo objects
+   * @param  {[String]} airTablePromoCode Unique AirTable promo code acting as ID
+   * @return {[Array]}                    List of unseen promo objects to be sent to airtable
+   */
   removeDuplicatePromo(promos, airTablePromoCode) {
-    return promos.filter(function(promo) {
-      if (promo.promo_code) {
-        return promo.promo_code !== airTablePromoCode;
+    return promos.filter(function(promoObj) {
+      if (promoObj.promoObj.promo_code[0]) {
+        return promoObj.promoObj.promo_code[0] !== airTablePromoCode;
       }
     });
   }
